a7ded48a94da97a59c70c147db86bdd58fb53f25
Caramel <44335015+CaramelSweet@users.noreply.github.com>
2026-05-25 14:55:59 -0400

    Finish exercise

--- Exercise 1-17.c
+++ Exercise 1-17.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#define MAXLINE 1000
+#define PLENGTH 80
+
+int get_line(char line[], int maxLine);
 
 int main (void) {
+    int lineLength;
+    char line[MAXLINE];
 
+    while ((lineLength = get_line(line, MAXLINE)) > 0) {
+        if (lineLength >= PLENGTH) {
+            printf("%s", line);
+        }
+    }
+    
+    return 0;
 }
 
-int printStuff(char arr[], int lim) {
+int get_line(char line[], int maxLine) {
+    int c, i;
 
+    i = 0;
+    while ((c = getchar()) != EOF && c != '\n') {
+        if (i < maxLine - 1) {
+            line[i] = c;
+        }
+        ++i;
+    }
+
+    if (c == '\n') {
+        if (i < maxLine - 1) {
+        line[i] = c;
+        }
+        ++i;
+    }
+    
+    if (i < maxLine) {
+        line[i] = '\0';
+    } else {
+        line[maxLine - 1] = '\0';
+    }
+
+    return i;
 }
