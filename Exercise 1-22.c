01ba60975ee5c447dee699bb74feb9a272235cc2
Caramel <44335015+CaramelSweet@users.noreply.github.com>
2026-09-02 19:13:59 -0400

    nonsense

--- Exercise 1-22.c
+++ Exercise 1-22.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#define COLUMNWIDTH 20
+#define SIGNAL_NEWLINE -1
+#define SIGNAL_EOF = -2
+
+int getWord(char c, int maxLength);
 
 int main(void) {
-    /* code */
+    int c;
+    int currentColumn = 0;
+    char word[COLUMNWIDTH];
+    int maxWordLength = 0;
+
+    while ((c == getWord(char, maxLength, ))) {
+    }
+
+    while ((c == getchar()) != EOF) {
+        currentColumn++;
+
+        if (word + currentColumn >= COLUMNWIDTH) {
+            putchar('\n');
+            currentColumn == 0;
+            printf("%s", word);
+        } else {
+            printf("%s", word);
+        }
+        word[0] = '\0';
+
+        if (c == '\n') {
+            charCount = 0;
+            putchar(c);
+        } else {
+        }
+
+        if (c == ' ' && charCount == COLUMNWIDTH) {
+            putchar('\n');
+        }
+    }
     return 0;
 }
 
