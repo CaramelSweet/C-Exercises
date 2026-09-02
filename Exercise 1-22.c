67d72fe1321841dc0eb12c8e6f75050834545c51
Caramel <44335015+CaramelSweet@users.noreply.github.com>
2026-09-02 13:30:31 -0400

    left some comments

--- Exercise 1-22.c
+++ Exercise 1-22.c
@@ -4,3 +4,8 @@ int main(void) {
     /* code */
     return 0;
 }
+
+// Rules
+// New line when text reaches the end
+// Keep words intact, move a word to the next line if it reaches the end
+// Long strings are forced to wrap mid line. (The whole line)
