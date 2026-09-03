dc12679c57f86b834c73e86891f9e981b63021e4
Caramel <44335015+CaramelSweet@users.noreply.github.com>
2026-09-02 22:41:33 -0400

    1-22 sync

--- Exercise 1-22.c
+++ Exercise 1-22.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
-#define COLUMNWIDTH 20
-#define SIGNAL_NEWLINE -1
-#define SIGNAL_EOF = -2
+#define COLWIDTH 20
 
-int getWord(char c, int maxLength);
+int getWord();
+
+char word[COLWIDTH];
+char endOfWord;
 
 int main(void) {
-    int c;
-    int currentColumn = 0;
-    char word[COLUMNWIDTH];
-    int maxWordLength = 0;
+    int colPos = 0;
+    int wordLength;
 
-    while ((c == getWord(char, maxLength, ))) {
+    while (() {
+        if (result ==)
+
+            if (wordLength + colPos > COLWIDTH) {
+                putchar('\n');
+                colPos = 0;
+            }
+        printf("%s", word);
     }
 
-    while ((c == getchar()) != EOF) {
-        currentColumn++;
-
-        if (word + currentColumn >= COLUMNWIDTH) {
-            putchar('\n');
-            currentColumn == 0;
-            printf("%s", word);
-        } else {
-            printf("%s", word);
-        }
-        word[0] = '\0';
-
-        if (c == '\n') {
-            charCount = 0;
-            putchar(c);
-        } else {
-        }
-
-        if (c == ' ' && charCount == COLUMNWIDTH) {
-            putchar('\n');
-        }
-    }
     return 0;
+}
+
+int getWord() {
+    int c;
+    int i = 0;
+
+    while ((c = getchar()) != EOF || c != '\n' || c != ' ') {
+        i[word] = c;
+    }
+    return i;
+    endOfWord = c;
 }
 
 // Rules
