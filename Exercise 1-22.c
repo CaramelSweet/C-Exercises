5cf5914aa22291fc0f4f0cd24a91539829c68791
Caramel <44335015+CaramelSweet@users.noreply.github.com>
2026-09-03 16:08:34 -0400

    mostly works

--- Exercise 1-22.c
+++ Exercise 1-22.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
-#define COLWIDTH 20
+#define COLWIDTH 10
+#define TABSIZE 4
 
 int getWord();
-
-char word[COLWIDTH];
-char endOfWord;
+char word[COLWIDTH + 1];
+int endOfWord = 0;
 
 int main(void) {
+    setvbuf(stdout, NULL, _IONBF, 0);
     int colPos = 0;
     int wordLength;
 
-    while (() {
-        if (result ==)
+    while (endOfWord != EOF) {
+        wordLength = getWord();
 
-            if (wordLength + colPos > COLWIDTH) {
-                putchar('\n');
-                colPos = 0;
-            }
+        if (wordLength + colPos > COLWIDTH) {
+            putchar('\n');
+            colPos = 0;
+        }
+
         printf("%s", word);
+        colPos += wordLength;
+
+        if (endOfWord == ' ' && colPos < COLWIDTH) {
+            putchar(' ');
+            colPos++;
+        } else if (endOfWord == '\n') {
+            putchar('\n');
+            colPos = 0;
+        } else if (endOfWord == '\t' && colPos <= COLWIDTH) {
+            int spaceLeft = COLWIDTH - colPos;
+            int spacesToTab = TABSIZE - (colPos % TABSIZE);
+            int numSpaces;
+
+            if (spaceLeft > spacesToTab) {
+                numSpaces = spacesToTab;
+            } else {
+                numSpaces = spaceLeft;
+            }
+
+            int i = 0;
+            while (i < numSpaces) {
+                putchar('T');
+                i++;
+            }
+
+            colPos += numSpaces;
+        }
     }
 
     return 0;
@@ -27,13 +56,21 @@ int getWord() {
     int c;
     int i = 0;
 
-    while ((c = getchar()) != EOF || c != '\n' || c != ' ') {
-        i[word] = c;
+    while (i < COLWIDTH && (c = getchar()) != EOF && c != '\n' && c != ' ' && c != '\t') {
+        word[i] = c;
+        i++;
     }
-    return i;
-    endOfWord = c;
-}
 
+    if (i >= COLWIDTH) {
+        endOfWord = 0;
+    } else {
+        endOfWord = c;
+    }
+
+    word[i] = '\0';
+
+    return i;
+}
 // Rules
 // New line when text reaches the end
 // Keep words intact, move a word to the next line if it reaches the end
